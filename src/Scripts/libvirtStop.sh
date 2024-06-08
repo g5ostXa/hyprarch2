9025a64fb65bdc914dd85cf37d2bb72a9b19d984
g5ostX2 <g5ostX2@proton.me>
2024-06-08 16:50:32 -0400

    Update

--- src/Scripts/libvirtStop.sh
+++ src/Scripts/libvirtStop.sh
@@ -2,11 +2,13 @@
 
 function_1() {
 
-sudo virsh net-destroy default && killall virt-manager;
+sudo virsh net-destroy default;
+killall virt-manager;
 sudo systemctl disable --now libvirtd.service;
 sudo systemctl disable --now libvirtd.socket;
 sudo systemctl disable --now libvirtd-admin.socket;
 sudo systemctl disable --now libvirtd-ro.socket;
+sudo firecfg;
 
 }
 
