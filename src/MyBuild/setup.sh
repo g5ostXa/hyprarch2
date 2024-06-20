18975e4c71735ca43ce367fd6b79a36e791cd2b8
g5ostX2 <g5ostX2@proton.me>
2024-06-20 16:06:07 -0400

    setup.sh update

--- src/MyBuild/setup.sh
+++ src/MyBuild/setup.sh
@@ -2,38 +2,50 @@
 
 dotfiles_dir="$HOME/dotfiles/"
 
-# Create dotfiles directory if needed
-function_1() {
-
-if [ -d "$dotfiles_dir" ]; then
-    echo "dotfiles directory already exists, aborting!"
-else
-    echo "Creating dotfiles directory..." | lolcat
-    sleep 2
-    mkdir -p "$dotfiles_dir"
-    echo "dotfiles directory created!" | lolcat
-    sleep 2
-fi
-
-}
-
-# Copying files and directories
-function_2() {
-
-rm -rf ~/.config/fish/
-rm -rf ~/.config/autostart/
-rm -rf ~/.bashrc
-rm -rf ~/.icons
-cp -r "$HOME"/Downloads/hyprarch2/.config/* "$HOME"/.config/
-cp -r "$HOME"/Downloads/hyprarch2/.bashrc ~/
-cp -r "$HOME"/Downloads/hyprarch2/dotfiles/* ~/dotfiles
-cp -r "$HOME"/Downloads/hyprarch2/wallpaper/ ~/
-cp -r "$HOME"/Downloads/hyprarch2/src/ ~/
-cp -r "$HOME"/Downloads/hyprarch2/docs/ ~/
-cp -r "$HOME"/Downloads/hyprarch2/.icons/ ~/
+create_dotfiles_dir() {
+    if [ -d "$dotfiles_dir" ]; then
+        echo "dotfiles directory already exists, aborting!"
+        exit 1
+    else
+        echo "Creating dotfiles directory..." | lolcat
+        sleep 2
+        mkdir -p "$dotfiles_dir"
+        echo "dotfiles directory created!" | lolcat
+        sleep 2
+    fi
 
 }
 
-function_1
-function_2
+remove_files() {
+    local paths=(
+        "$HOME/.config/fish/"
+        "$HOME/.config/autostart/"
+        "$HOME/.bashrc"
+        "$HOME/.icons"
+    )
 
+    for path in "${paths[@]}"; do
+        if [ -e "$path" ]; then
+            rm -rf "$path"
+            echo "Removed $path"
+        else
+            echo "Path $path does not exist"
+        fi
+    done
+
+}
+
+copy_files() {
+    cp -r "$HOME/Downloads/hyprarch2/.config/"* "$HOME/.config/"
+    cp -r "$HOME/Downloads/hyprarch2/.bashrc" "$HOME/"
+    cp -r "$HOME/Downloads/hyprarch2/dotfiles/"* "$HOME/dotfiles/"
+    cp -r "$HOME/Downloads/hyprarch2/wallpaper/" "$HOME/"
+    cp -r "$HOME/Downloads/hyprarch2/src/" "$HOME/"
+    cp -r "$HOME/Downloads/hyprarch2/docs/" "$HOME/"
+    cp -r "$HOME/Downloads/hyprarch2/.icons/" "$HOME/"
+
+}
+
+create_dotfiles_dir
+remove_files
+copy_files
