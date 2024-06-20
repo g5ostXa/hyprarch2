c51360944662d181f8f01e0afa6ec37e6a2e7631
g5ostX2 <g5ostX2@proton.me>
2024-06-20 17:47:20 -0400

    added variable

--- src/MyBuild/install-packages.sh
+++ src/MyBuild/install-packages.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 packages="$HOME/Downloads/hyprarch2/packages/pacman_packages.txt"
+aur_helper="paru"
 
 install_packages() {
 
 sudo pacman -Syu && sudo pacman -S --needed - < "$packages"
-cd "$HOME"; git clone https://aur.archlinux.org/paru.git 
-cd "$HOME/paru" && makepkg -si
+cd "$HOME"; git clone https://aur.archlinux.org/$aur_helper.git 
+cd "$HOME/$aur_helper/" && makepkg -si
 
 if [ $? -eq 0 ]; then
-    echo "Paru successfully built! Moving on..."
+    echo "$aur_helper successfully built! Moving on..."
 else
-    echo "Paru failed to build... Exiting script"
+    echo "$aur_helper failed to build... Exiting script"
     exit 1
 fi
 
-paru -S bibata-cursor-theme dracula-gtk-theme swaylock-effects swww trizen wlogout wlr-randr
+$aur_helper -S bibata-cursor-theme dracula-gtk-theme swaylock-effects swww trizen wlogout wlr-randr
 
 }
 
