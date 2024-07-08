7705c6a06bdda32202a2f738c9d178c834e8c2ab
g5ostX2 <g5ostX2@proton.me>
2024-07-08 09:31:38 -0400

    Fixed setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -11,7 +11,7 @@ packages="$HOME/Downloads/hyprarch2/packages/pacman_packages.txt"
 aur_helper="paru-bin"
 
 install_packages() {
-sudo pacman -Syu && sudo pacman -S --needed --noconfirm - < "$packages"
+sudo pacman -Syu && sudo pacman -S --needed - < "$packages"
 cd "$HOME"; git clone https://aur.archlinux.org/$aur_helper.git 
 cd "$HOME/$aur_helper/" && makepkg -si
 
