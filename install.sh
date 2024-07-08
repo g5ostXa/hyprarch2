35da5cb8a236aab2b8ece83194a11f5bacba8506
g5ostX2 <g5ostX2@proton.me>
2024-07-08 12:46:40 -0400

    Merged installer.sh with install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RC='\033[0m'
+
+echo -e "${GREEN}"
+cat <<"EOF"
+ ___           _        _ _
+|_ _|_ __  ___| |_ __ _| | | ___ _ __
+ | || '_ \/ __| __/ _` | | |/ _ \ '__|
+ | || | | \__ \ || (_| | | |  __/ |
+|___|_| |_|___/\__\__,_|_|_|\___|_|
+
+EOF
+echo "Welcome to hyprarch2"
+echo
+echo -e "${RC}"
+echo
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo ":: Installation started."
+            echo
+        break;;
+        [Nn]* )
+            echo ":: Installation canceled."
+            exit;
+        break;;
+        * ) echo ":: Please answer yes or no.";;
+    esac
+done
+
 set -e
 
 main_vars="$HOME/Downloads/hyprarch2/src/variables.sh"
