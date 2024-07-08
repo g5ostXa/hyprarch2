447946ec5242135d0114e7f1d20c392a2d2bd67d
g5ostX2 <g5ostX2@proton.me>
2024-07-08 13:16:57 -0400

    update

--- src/Scripts/checkerrors.sh
+++ src/Scripts/checkerrors.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+CYAN='\033[0;36m'
 YELLOW='\033[0;33m'
 RC='\033[0m' 
 
-echo CheckErrors | figlet
+echo -e "${CYAN}"
+cat <<"EOF"
+  ____ _               _    _____
+ / ___| |__   ___  ___| | _| ____|_ __ _ __ ___  _ __ ___
+| |   | '_ \ / _ \/ __| |/ /  _| | '__| '__/ _ \| '__/ __|
+| |___| | | |  __/ (__|   <| |___| |  | | | (_) | |  \__ \
+ \____|_| |_|\___|\___|_|\_\_____|_|  |_|  \___/|_|  |___/
+
+EOF
+echo -e "${RC}"
 
 check_errors() {
 echo -e "${YELLOW}Checking for system errors...${RC}"
