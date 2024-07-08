637f0138f6fc42ae4cde2a189ff9bf995accddc1
g5ostX2 <g5ostX2@proton.me>
2024-07-08 13:53:33 -0400

    update

--- install.sh
+++ install.sh
@@ -21,17 +21,18 @@ while true; do
     read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
     case $yn in
         [Yy]* )
-            echo ":: Installation started."
+            echo ":: Starting Installation..."
             echo
         break;;
         [Nn]* )
-            echo ":: Installation canceled."
+            echo ":: Installation Canceled."
             exit;
         break;;
         * ) echo ":: Please answer yes or no.";;
     esac
 done
 
+sleep 2
 set -e
 
 main_vars="$HOME/Downloads/hyprarch2/src/variables.sh"
