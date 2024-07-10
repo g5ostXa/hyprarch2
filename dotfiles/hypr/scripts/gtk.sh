a5e340a485b6d6edbfc8ec0ca5d19192169d08eb
g5ostX2 <g5ostX2@proton.me>
2024-07-10 16:28:34 -0400

    GTK updates

--- dotfiles/hypr/scripts/gtk.sh
+++ dotfiles/hypr/scripts/gtk.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+config="$HOME/.config/gtk-3.0/settings.ini" 
+if [ ! -f "$config" ]; then exit 1; fi 
+
 gnome_schema="org.gnome.desktop.interface"
+gsettings set "$gnome_schema" gtk-theme "Dracula"
 gsettings set "$gnome_schema" icon-theme "Papirus-Dark"
 gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Ice"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
