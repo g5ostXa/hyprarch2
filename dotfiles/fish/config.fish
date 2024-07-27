94980f18c076e3881eabef3e0b4e378c9851c0d5
g5ostX2 <g5ostX2@proton.me>
2024-07-27 09:40:11 -0400

    update

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,7 +2,8 @@ if status is-interactive
 
 # ALIASES
 alias ls='lsd'
-alias ll='lsd -al'
+alias ll='lsd -a'
+alias ls -al='lsd -al'
 alias vpn-connect='sudo protonvpn connect -f'
 alias vpn-disconnect='sudo protonvpn disconnect'
 alias vpn-reconnect='sudo protonvpn reconnect'
