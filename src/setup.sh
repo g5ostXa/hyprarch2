603eac70eb104c5ce3001f865e1e203461d68419
g5ostX2 <g5ostX2@proton.me>
2024-09-09 04:25:38 -0400

    update

--- src/setup.sh
+++ src/setup.sh
@@ -130,6 +130,14 @@ copy_files() {
 		cp -r "$HOME/Downloads/hyprarch2/.version/" "$HOME/"
 	fi
 
+	# Temporary fix for issue (#30)
+	if [ -f "$HOME/.gitignore" ]; then
+		echo -e "${YELLOW}$HOME/.gitignore exists...${RC}"
+	else
+		echo -e "${YELLOW}Copying .gitignore to home folder...${RC}"
+		cp -r "$HOME/Downloads/hyprarch2/.gitignore" "$HOME/"
+	fi
+
 	sleep 2
 
 }
