8a51886f8d7053133be2ee79a7e44c7f05140e1b
Dvnny <90010982+g5ostXa@users.noreply.github.com>
2024-10-13 17:24:52 +0000

    Update keybindings.sh

--- src/Scripts/keybindings.sh
+++ src/Scripts/keybindings.sh
@@ -4,7 +4,7 @@
 # keybindings.sh
 # -------------------------------------------------------------------------------------
 
-config_keybinds() {
+keybinds_rofi() {
 	config_file=~/dotfiles/hypr/conf/keybindings.conf
 	keybinds=$(grep -oP '(?<=bind = ).*' $config_file)
 	keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g' | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
@@ -12,4 +12,4 @@ config_keybinds() {
 
 }
 
-config_keybinds
+keybinds_rofi
