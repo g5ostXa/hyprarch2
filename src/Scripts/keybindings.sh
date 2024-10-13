5891913a27d1d46896ccf87c6d8042ddd91bc192
Dvnny <90010982+g5ostXa@users.noreply.github.com>
2024-10-13 05:49:59 +0000

    Update keybindings.sh

--- src/Scripts/keybindings.sh
+++ src/Scripts/keybindings.sh
@@ -6,16 +6,12 @@
 # |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
 #           |___/                             |___/
 
+CONFIG_KEYBINDS() {
+  binds_conf="~/dotfiles/hypr/conf/keybindings.conf"
+  keybinds=$(grep -oP '(?<=bind = ).*' $binds_conf)
+  keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g' | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
+  rofi -dmenu -replace -p "Keybinds" -config ~/dotfiles/rofi/config-compact.rasi <<<"$keybinds"
 
-binds_conf="~/dotfiles/hypr/conf/keybindings.conf"
+}
 
-# -----------------------------------------------------
-# Parse keybindings
-# -----------------------------------------------------
-keybinds=$(grep -oP '(?<=bind = ).*' $binds_conf)
-keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g' | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
-
-# -----------------------------------------------------
-# Show keybindings in rofi
-# -----------------------------------------------------
-rofi -dmenu -replace -p "Keybinds" -config ~/dotfiles/rofi/config-compact.rasi <<<"$keybinds"
+config_keybinds
