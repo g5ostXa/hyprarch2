b715366582375fb775e06cd72de8d18399369707
Dvnny <90010982+g5ostXa@users.noreply.github.com>
2024-10-13 06:25:51 +0000

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
-#                _ _
-# __      ____ _| | |_ __   __ _ _ __   ___ _ __
-# \ \ /\ / / _` | | | '_ \ / _` | '_ \ / _ \ '__|
-#  \ V  V / (_| | | | |_) | (_| | |_) |  __/ |
-#   \_/\_/ \__,_|_|_| .__/ \__,_| .__/ \___|_|
-#                   |_|         |_|
-#
-# -----------------------------------------------------
-# Get selected wallpaper
-# -----------------------------------------------------
-wallpaper="$1"
-used_wallpaper="$wallpaper"
 
-# -----------------------------------------------------
-# Execute pywal
-# -----------------------------------------------------
-wal -q -i "$used_wallpaper"
-source "$HOME/.cache/wal/colors.sh"
+#-----------------------------------------------------
+# walppaper.sh
+# ----------------------------------------------------
 
-# -----------------------------------------------------
-# Copy selected wallpaper into .cache folder
-# -----------------------------------------------------
-cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
+wall_select_and_colors() {
+    # Define selected wallpaper
+    wallpaper="$1"
+    used_wallpaper="$wallpaper"
 
-# -----------------------------------------------------
-# Get wallpaper image name
-# -----------------------------------------------------
-newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+    # Apply pywal colors
+    wal -q -i "$used_wallpaper"
+    source "$HOME/.cache/wal/colors.sh"
 
-# -----------------------------------------------------
-# Reload Waybar
-# -----------------------------------------------------
-source "$HOME/dotfiles/waybar/launch.sh"
+}
 
-# -----------------------------------------------------
-# Send notification
-# -----------------------------------------------------
-sleep 1
-notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
+cache_and_waybar() {
+    # Copy selected wallpaper to ~/.cache/
+    cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
+
+    # Get new wallpaper's name
+    newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+
+    # Reload waybar
+    source "$HOME/dotfiles/waybar/launch.sh"
+
+    # Send notification
+    sleep 1
+    notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
+
+}
+
+wall_select_and_colors
+cache_and_waybar
