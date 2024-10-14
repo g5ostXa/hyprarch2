6bc74683bc08354a5b5abf3dbd4f93ed51adda42
g5ostX2 <g5ostX2@proton.me>
2024-10-14 13:04:00 -0400

    Added hyprilde.sh

---
+++ src/Scripts/hypridle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ----------------------------------------------------
+# hypridle.sh
+# ----------------------------------------------------
+
+SERVICE="hypridle"
+
+idle_toggle() {
+	if pgrep -x "$SERVICE" >/dev/null; then
+		killall "$SERVICE"
+		notify-send --urgency=normal ":: $SERVICE DEACTIVATED"
+	else
+		"$SERVICE" &
+		notify-send --urgency=normal ":: $SERVICE ACTIVATED"
+	fi
+
+}
+
+idle_toggle
