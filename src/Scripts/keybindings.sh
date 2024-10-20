fdf969ebff8bf2fd57fd9d9a9a25c7cbf4afac3d
g5ostX2 <g5ostX2@proton.me>
2024-10-19 23:31:26 -0400

    Update keybindings.sh

--- src/Scripts/keybindings.sh
+++ src/Scripts/keybindings.sh
@@ -4,15 +4,10 @@
 # keybindings.sh
 # -------------------------------------------------------------------------------------
 
-keybinds_rofi() {
-	# Load keybinds lists for rofi
-	config_file=~/dotfiles/hypr/conf/keybindings.conf
-	keybinds=$(grep -oP '(?<=bind = ).*' $config_file)
-	keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g' | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
+# Parse and load keybinds
+config_file=~/dotfiles/hypr/conf/keybindings.conf
+keybinds=$(grep -oP '(?<=bind = ).*' $config_file)
+keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g' | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
 
-	# Show keybinds in rofi
-	rofi -dmenu -replace -p "Keybinds" -config ~/dotfiles/rofi/config-compact.rasi <<<"$keybinds"
-
-}
-
-keybinds_rofi
+# List all available keybinds in rofi
+rofi -dmenu -replace -p "Keybinds" -config ~/dotfiles/rofi/config-compact.rasi <<<"$keybinds"
