3c34aa14c2dc56a122f48fe56382110e9e8424fb
g5ostX2 <g5ostX2@proton.me>
2024-10-20 19:46:25 -0400

    Update mirrors.sh

--- src/Scripts/mirrors.sh
+++ src/Scripts/mirrors.sh
@@ -5,7 +5,7 @@
 # ---------------
 # By g5ostXa 2024
 # ---------------------------
-# Requires: reflector and gum
+# Requires: reflector gum figlet
 # ----------------------------------------------------
 
 # Define variables
@@ -13,6 +13,7 @@ MIRRORLIST="/etc/pacman.d/mirrorlist"
 MIRRORLIST_BAK="/etc/pacman.d/mirrorlist.bak"
 REFLECTOR_CMD="sudo reflector --country Canada --protocol https --latest 20 --age 6 --sort rate --save $MIRRORLIST"
 SERVICE="openvpn"
+VPN_NAME="protonvpn"
 VPN_CONNECT="sudo protonvpn connect -f"
 VPN_DISCONNECT="sudo protonvpn disconnect"
 SYNC_DATABASE="sudo pacman -Syu"
@@ -22,21 +23,27 @@ create_mirrorlist() {
 	if pgrep -x "$SERVICE" >/dev/null; then
 		gum spin --spinner points --title "Disconnecting VPN" -- $VPN_DISCONNECT
 	fi
-	
-	gum spin --spinner globe --title "Fetching latest mirrors..." -- $REFLECTOR_CMD
-	gum spin --spinner points --title "Synchronizing package database..." -- $SYNC_DATABASE
-	
-	if ! pgrep -x "$SERVICE" >/dev/null; then
-		gum spin --spinner points --title "Reconnecting VPN..." -- $VPN_CONNECT
-	fi
 
-	echo "The mirrors are now up to date!"
+	gum spin --spinner globe --title "Fetching the latest mirrors..." -- $REFLECTOR_CMD
+	gum spin --spinner points --title "Synchronizing package database..." -- $SYNC_DATABASE
+
+	if ! pgrep -x "$SERVICE" >/dev/null; then
+		if command -v "$VPN_NAME" >/dev/null 2>&1; then
+			gum spin --spinner points --title "Reconnecting VPN..." -- $VPN_CONNECT
+			echo "The mirrors are now up to date!"
+		else
+			echo "The mirrors are now up to date!"
+		fi
+	fi
 
 }
 
 check_backup() {
 	if [ -f "$MIRRORLIST_BAK" ]; then
-		clear && echo "" | sudo tee -a >/dev/null
+		if command -v figlet >/dev/null 2>&1; then
+		figlet "Mirrorlist"
+	fi
+		echo "" | sudo tee -a >/dev/null
 		if gum confirm "Remove existing backup and create a fresh one?"; then
 			sudo rm -rf "$MIRRORLIST_BAK"
 			sudo cp -r "$MIRRORLIST" "$MIRRORLIST_BAK"
@@ -49,7 +56,11 @@ check_backup() {
 			create_mirrorlist
 		fi
 	else
-		clear && echo "" | sudo tee -a >/dev/null
+		if command -v figlet >/dev/null 2>&1; then
+			figlet "Mirrorlist"
+		fi
+
+		echo "" | sudo tee -a >/dev/null
 		if gum confirm "Do you want to create a backup of your current mirrorlist?"; then
 			sudo cp -r "$MIRRORLIST" "$MIRRORLIST_BAK"
 			echo "Saved $MIRRORLIST as $MIRRORLIST_BAK"
