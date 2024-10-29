64ef02ffc2062f3d4ba63ec015b537d061571fe8
g5ostXa <g5ostX2@proton.me>
2024-10-28 20:24:44 -0400

    shfmt update

--- src/Scripts/sandboxes/bwrap-btop.sh
+++ src/Scripts/sandboxes/bwrap-btop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 bwrap --ro-bind /usr /usr --ro-bind /bin /bin --ro-bind /lib /lib --ro-bind /lib64 /lib64 --ro-bind /sbin /sbin --ro-bind /etc /etc --dev /dev --proc /proc --tmpfs /tmp --ro-bind /home/$USER /home/$USER --bind /home/$USER/.config/btop /home/$USER/.config/btop --unshare-net --dir /run /usr/bin/btop
-
