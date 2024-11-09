3baa6e0cea2e71c56eb0b4cb6477058b0f97c111
g5ostXa <g5ostX2@proton.me>
2024-11-09 00:00:56 -0500

    Update

--- src/Scripts/wgstatus.sh
+++ src/Scripts/wgstatus.sh
@@ -7,15 +7,14 @@
 # Get wireguard status and basic info
 if [[ $(wg show | grep -c "peer:") -gt 0 ]]; then
 	echo ""
-    echo "VPN status:"
-    echo "connected"
-    echo ""
-    wg show
+	echo "VPN status:"
+	echo "connected"
+	echo ""
+	wg show
 else
-    echo ""
-    echo "VPN status:"
+	echo ""
+	echo "VPN status:"
 	echo "disconnected"
-    echo ""
-    wg show
+	echo ""
+	wg show
 fi
-
