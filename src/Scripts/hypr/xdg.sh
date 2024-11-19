20c531d026e1da1d5a138acc06c25cd7a9aa7475
g5ostXa <g5ostX2@proton.me>
2024-11-18 19:34:30 -0500

    Update xdg.sh

--- src/Scripts/hypr/xdg.sh
+++ src/Scripts/hypr/xdg.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-
-# --------------------------------------------------
-# xdg.sh
-# --------------------------------------------------
+# __  ______   ____
+# \ \/ /  _ \ / ___|
+#  \  /| | | | |  _
+#  /  \| |_| | |_| |
+# /_/\_\____/ \____|
+#
 
 sleep 1
 
 # kill all possible running xdg-desktop-portals
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gtk
-killall xdg-desktop-portal
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
 sleep 1
 
 # start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
+
+# start xdg-desktop-portal-gtk
+if [ -f /usr/lib/xdg-desktop-portal-gtk ]; then
+	/usr/lib/xdg-desktop-portal-gtk &
+	sleep 1
+fi
 
 # start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
