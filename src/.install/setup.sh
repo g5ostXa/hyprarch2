9ccf5be50298ad868771ecf71e89ad46a2d65c27
g5ostXa <g5ostX2@proton.me>
2024-12-07 21:14:33 -0500

    Update setup.sh

--- src/.install/setup.sh
+++ src/.install/setup.sh
@@ -171,7 +171,6 @@ create_symlinks() {
 	ln -s ~/dotfiles/pacseek/ ~/.config/
 	ln -s ~/dotfiles/waypaper/ ~/.config/
 	ln -s ~/dotfiles/uwsm/ ~/.config/
- 	ln -s ~/dotfiles/systemd/ ~/.config/
 
 }
 
