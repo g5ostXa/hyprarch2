8d0a66440a81e7407a5f87f0c403fa38fe1828f9
g5ostXa <g5ostX2@proton.me>
2025-09-14 05:42:58 -0400

    Updates

--- src/Scripts/updates.sh
+++ src/Scripts/updates.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
-# Define some variables
+# Define waybar module color based on the number of updates
 threshold_green=0
 threshold_yellow=25
 threshold_red=100
 
 # Define which AUR helper is installed
-if command -v paru >/dev/null; then
-	AUR_HELPER="paru"
-else
-	if command -v yay >/dev/null; then
-		AUR_HELPER="yay"
+define_helper() {
+	if command -v paru >/dev/null; then
+		AUR_HELPER="paru"
 	else
-		if command -v notify-send >/dev/null; then
-			notify-send --urgency=critical "Neither paru or yay is installed..."
+		if command -v yay >/dev/null; then
+			AUR_HELPER="yay"
 		else
-			echo "Neither paru or yay is installed..."
-			exit 1
+			if command -v notify-send >/dev/null; then
+				notify-send --urgency=critical "Neither paru or yay is installed..."
+			else
+				echo "Neither paru or yay is installed..."
+				exit 1
+			fi
 		fi
 	fi
-fi
 
-# Get updates total number
-if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
-	updates_arch=0
-fi
+}
 
-if ! updates_aur=$("$AUR_HELPER" -Qua 2>/dev/null | wc -l); then
-	updates_aur=0
-fi
+# Get total number of updates
+get_number() {
+	if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
+		updates_arch=0
+	fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+	if ! updates_aur=$("$AUR_HELPER" -Qua 2>/dev/null | wc -l); then
+		updates_aur=0
+	fi
 
-css_class="green"
+	updates=$(("$updates_arch" + "$updates_aur")) && css_class="green"
 
+}
+
+define_helper && get_number
+
+# Print json format for waybar module
 if [ "$updates" -gt $threshold_yellow ]; then
 	css_class="yellow"
 fi
