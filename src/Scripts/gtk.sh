2c32943a6f3f32add6588e6a1fdb42bc2bdc084d
g5ostXa <g5ostX2@proton.me>
2025-09-14 10:59:20 -0400

    Update

--- src/Scripts/gtk.sh
+++ src/Scripts/gtk.sh
@@ -8,7 +8,7 @@ if [ ! -f "$config" ]; then exit 1; fi
 
 gnome_schema="org.gnome.desktop.interface"
 gsettings set "$gnome_schema" gtk-theme "Tokyonight-Dark"
-gsettings set "$gnome_schema" icon-theme "Tokyonight-Dark"
+gsettings set "$gnome_schema" icon-theme "Dracula"
 gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Ice"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
 gsettings set "$gnome_schema" color-scheme "prefer-dark"
