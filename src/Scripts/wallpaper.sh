a7b573d185d78af03a334e5b5fd77d631e14853e
g5ostXa <90010982+g5ostXa@users.noreply.github.com>
2025-09-15 19:55:29 -0400

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -15,17 +15,20 @@ main_function() {
 
 	# Get new wallpaper's name
 	newwall=$(echo "$wallpaper" | sed "s|$HOME/wallpaper/||g")
-
+	
+	# Load theme configuration
 	SETTINGS_FILE="$HOME"/.config/gtk-3.0/settings.ini
 	THEME_PREF=$(grep -E '^gtk-application-prefer-dark-theme=' "$SETTINGS_FILE" | awk -F'=' '{print $2}')
 	
+	# Set theme using matugen based on $used_wallpaper colors
 	if [ "$THEME_PREF" -eq 1 ]; then
-		"$HOME"/.local/bin/matugen image "$used_wallpaper" -m "dark"
-		else
-		"$HOME"/.local/bin/matugen image "$used_wallpaper" -m "light"
+			"$HOME"/.local/bin/matugen image "$used_wallpaper" -m "dark"
+	else
+		 "$HOME"/.local/bin/matugen image "$used_wallpaper" -m "light"
 	fi
 	
- # command to set wallpaper here
+	# Set wallpaper
+ swww img "$used_wallpaper" --transition-type center
 	
 	# Send notification only if waybar is running to prevent a wallpaper update notification every time hyprland is launched
 	if pgrep -x waybar >/dev/null; then
