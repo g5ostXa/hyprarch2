f726d5f6390765874341bed173da6d36582f0cdf
g5ostXa <90010982+g5ostXa@users.noreply.github.com>
2025-09-15 19:24:24 -0400

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -9,9 +9,6 @@ wallpaper="$1"
 used_wallpaper="$wallpaper"
 
 main_function() {
-	# Apply pywal colors
-	wal -q -i "$used_wallpaper"
-	source "$HOME/.cache/wal/colors.sh"
 
 	# Copy selected wallpaper to ~/.cache/
 	cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
@@ -19,17 +16,24 @@ main_function() {
 	# Get new wallpaper's name
 	newwall=$(echo "$wallpaper" | sed "s|$HOME/wallpaper/||g")
 
+	SETTINGS_FILE="$HOME"/.config/gtk-3.0/settings.ini
+	THEME_PREF=$(grep -E '^gtk-application-prefer-dark-theme=' "$SETTINGS_FILE" | awk -F'=' '{print $2}')
+	
+	if [ "$THEME_PREF" -eq 1 ]; then
+    "$HOME"/.local/bin/matugen image "$used_wallpaper" -m "dark"
+	else
+    "$HOME"/.local/bin/matugen image "$used_wallpaper" -m "light"
+	fi
+	
+	waypaper
+	
 	# Send notification only if waybar is running to prevent a wallpaper update notification every time hyprland is launched
 	if pgrep -x waybar >/dev/null; then
 		notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
 	fi
-
-    SETTINGS_FILE="$HOME/.config/gtk-3.0/settings.ini"
-    THEME_PREF=$(grep -E '^gtk-application-prefer-dark-theme=' "$SETTINGS_FILE" | awk -F'=' '{print $2}')
-    "$HOME"/.local/bin/matugen image "$used_wallpaper" -m "dark"
-
+	
 	# Reload waybar
-	source "$HOME/src/Scripts/waybar/launch.sh"
+	source "$HOME"/src/Scripts/waybar/launch.sh
 
 }
 
