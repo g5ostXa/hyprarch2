b7a8641d27421bdfa14b0afbb90c780fbc4e6536
g5ostXa <90010982+g5ostXa@users.noreply.github.com>
2025-09-15 18:44:02 -0400

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -24,6 +24,10 @@ main_function() {
 		notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
 	fi
 
+    SETTINGS_FILE="$HOME/.config/gtk-3.0/settings.ini"
+    THEME_PREF=$(grep -E '^gtk-application-prefer-dark-theme=' "$SETTINGS_FILE" | awk -F'=' '{print $2}')
+    "$HOME"/.local/bin/matugen image $used_wallpaper -m "dark"
+
 	# Reload waybar
 	source "$HOME/src/Scripts/waybar/launch.sh"
 
