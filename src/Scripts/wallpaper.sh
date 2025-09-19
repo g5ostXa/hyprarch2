5b8ed99fa33cddade0a69ada6bd8d8837d036f13
g5ostXa <g5ostX2@proton.me>
2025-09-19 16:42:22 -0400

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -8,25 +8,20 @@
 wallpaper="$1"
 used_wallpaper="$wallpaper"
 
-main_function() {
-	# Apply pywal colors
-	wal -q -i "$used_wallpaper"
-	source "$HOME/.cache/wal/colors.sh"
+# Apply pywal colors
+wal -q -i "$used_wallpaper" && swww img "$used_wallpaper" --transition-type outer --transition-step 90 --transition-fps 60
+source "$HOME/.cache/wal/colors.sh"
 
-	# Copy selected wallpaper to ~/.cache/
-	cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
+# Copy selected wallpaper to ~/.cache/
+cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
 
-	# Get new wallpaper's name
-	newwall=$(echo "$wallpaper" | sed "s|$HOME/wallpaper/||g")
+# Get new wallpaper's name
+newwall=$(echo "$wallpaper" | sed "s|$HOME/wallpaper/||g")
 
-	# Send notification only if waybar is running to prevent a wallpaper update notification every time hyprland is launched
-	if pgrep -x waybar >/dev/null; then
-		notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
-	fi
+# Send notification only if waybar is running to prevent a wallpaper update notification every time hyprland is launched
+if pgrep -x waybar >/dev/null; then
+	notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
+fi
 
-	# Reload waybar
-	source "$HOME/src/Scripts/waybar/launch.sh"
-
-}
-
-main_function
+# Reload waybar
+source "$HOME/src/Scripts/waybar/launch.sh"
