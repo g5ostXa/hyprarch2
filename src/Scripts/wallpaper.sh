184632e31717771b84c7c150531734da17a54f96
g5ostXa <g5ostX2@proton.me>
2025-09-20 19:45:04 -0400

    Updates

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -9,7 +9,7 @@ wallpaper="$1"
 used_wallpaper="$wallpaper"
 
 # Apply pywal colors
-wal -q -i "$used_wallpaper" && swww img "$used_wallpaper" --transition-type outer --transition-step 90 --transition-fps 60
+wal -q -i "$used_wallpaper"
 source "$HOME/.cache/wal/colors.sh"
 
 # Copy selected wallpaper to ~/.cache/
@@ -18,10 +18,8 @@ cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
 # Get new wallpaper's name
 newwall=$(echo "$wallpaper" | sed "s|$HOME/wallpaper/||g")
 
-# Send notification only if waybar is running to prevent a wallpaper update notification every time hyprland is launched
-if pgrep -x waybar >/dev/null; then
-	notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
-fi
+# Send notification
+notify-send --icon=/usr/share/icons/Dracula/16/folder-pictures.svg "Wallpaper and colors updated!" "with image $newwall"
 
 # Reload waybar
 source "$HOME/src/Scripts/waybar/launch.sh"
