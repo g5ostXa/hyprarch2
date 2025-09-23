ee447f4f51631d4af4bd89925c2733bc0753eb9c
g5ostXa <g5ostX2@proton.me>
2025-09-23 03:39:37 -0400

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# ---------------------------------
+# ------------------------------------------------
 # install.sh
-# ---------------------------------
+# ------------------------------------------------
 
 # Color Variables
 RED='\033[0;31m'
@@ -12,43 +12,61 @@ RC='\033[0m'
 # hyprarch2 source directory
 HYPRARCH2_DIR="$HOME/Downloads/hyprarch2"
 
-set -e
+# Check if hyprarch2 directory exists
+src_check() {
+	if [ ! -d "$HYPRARCH2_DIR" ]; then
+		echo -e "${RED};; $HYPRARCH2_DIR does not exist, exiting...${RC}"
+		exit 1
+	fi
 
-# Ensure hyprarch2 directory exists
-if [ ! -d "$HYPRARCH2_DIR" ]; then
-	echo -e "${RED};; $HYPRARCH2_DIR does not exist, exiting...${RC}"
-	exit 1
-fi
+}
 
 # Copy all hyprarch2 files to home directory
-cp -r "$HYPRARCH2_DIR"/* "$HOME/"
+src_copy() {
+	cp -r "$HYPRARCH2_DIR"/* "$HOME/"
+	sudo cp -r "$HYPRARCH2_DIR/dotfiles/login/issue" "/etc/"
 
-# Copy issue file to /etc
-sudo cp -r "$HYPRARCH2_DIR/dotfiles/login/issue" "/etc/"
-sudo chown root:root /etc/issue
+}
 
-# Check .bashrc exists
-if [ ! -f "$HOME/.bashrc" ]; then
-	echo -e "${YELLOW};; Copying .bashrc to home folder...${RC}"
-	cp "$HYPRARCH2_DIR/.bashrc" "$HOME/"
-fi
+# Make sure all files were copied
+target_check() {
+	if [ ! -f "$HOME/.bashrc" ]; then
+		echo -e "${YELLOW};; Copying .bashrc to home folder...${RC}"
+		cp "$HYPRARCH2_DIR/.bashrc" "$HOME"
+	fi
 
-# Check .version/ exists
-if [ ! -d "$HOME/.version/" ]; then
-	cp -r "$HYPRARCH2_DIR/.version/" "$HOME/"
-fi
+	# Check .version/ exists
+	if [ ! -d "$HOME/.version/" ]; then
+		cp -r "$HYPRARCH2_DIR/.version/" "$HOME"
+	fi
 
-# Check .github/ exists
-if [ ! -d "$HOME/.github/" ]; then
-	cp -r "$HYPRARCH2_DIR/.github/" "$HOME/"
-fi
+	# Check .github/ exists
+	if [ ! -d "$HOME/.github/" ]; then
+		cp -r "$HYPRARCH2_DIR/.github/" "$HOME"
+	fi
 
-# Check .gitignore exists
-if [ ! -f "$HOME/.gitignore" ]; then
-	cp "$HYPRARCH2_DIR/.gitignore" "$HOME/"
-fi
+	# Check .gitignore exists
+	if [ ! -f "$HOME/.gitignore" ]; then
+		cp "$HYPRARCH2_DIR/.gitignore" "$HOME"
+	fi
 
-sleep 2
+}
 
-cd "$HOME/Downloads" && git clone --depth=1 git@github.com:g5ostXa/h2install.git
-cd h2install && rm -rf .git/ && go mod tidy && go build -o h2installer && ./h2installer
+func_main() {
+	src_check && src_copy && target_check
+
+	if [ -f "/etc/issue" ]; then
+		sudo chown root:root /etc/issue
+	else
+		echo -e "${YELLOW}Failed to copy issue to /etc...${RC}"
+		sleep 1
+	fi
+
+	cd "$HOME/Downloads" && git clone --depth=1 git@github.com:g5ostXa/h2install.git
+	cd h2install && rm -rf .git/ && go mod tidy && go build -o h2installer && ./h2installer
+
+}
+
+# Script entry
+read -rp ";; Are you sure you want to start the installation now? [y/N]" ans
+[[ "$ans" =~ ^[Yy]$ ]] && func_main
