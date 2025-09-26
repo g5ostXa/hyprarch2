915b42420613bca88976fc93392b558b8bda2307
g5ostXa <g5ostX2@proton.me>
2025-09-26 16:01:34 -0400

    Updated the way we do wallpapers on h2!

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-#-----------------------------------------------------
-# wallpaper.sh
-#-----------------------------------------------------
+# ========= wallpaper.sh ==========
 
 wallpaper="$1"
 
+BAR="waybar"
+LAUNCH_BAR="$HOME/src/Scripts/waybar/launch.sh"
+
 [ -z "$wallpaper" ] && {
-	notify-send --urgency=critical "No wallpaper found" --icon=dialog-error
+	notify-send --urgency=critical "No wallpaper found..." --icon=dialog-error
 	exit 1
 }
 
-func_main() {
-	# Set wallpaper and colors
-	wal -q -i "$wallpaper"
-	cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
+# Set wallpaper and colors
+wal -q -i "$wallpaper"
+cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
 
-	# Get wallpaper name and send notification
-	newwall=$(basename "$wallpaper")
-	notify-send --urgency=normal "Wallpaper and colors updated!" "with image $newwall" --icon=folder-pictures
-}
+# Get wallpaper name and send notification
+newwall=$(basename "$wallpaper")
+notify-send --urgency=normal "Wallpaper and colors updated!" "with image $newwall" --icon=folder-pictures
 
-func_main
+# Reload waybar if needed
+sleep 0.25
+if ! pgrep -x "$BAR" >/dev/null; then "$LAUNCH_BAR"; fi
