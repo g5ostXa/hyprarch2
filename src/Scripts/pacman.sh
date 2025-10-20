37e7d88398971dd33451949eff29dfa48933b39e
g5ostXa <g5ostX2@proton.me>
2025-10-19 23:47:39 -0400

    Update pacman.sh for (#195)

--- src/Scripts/pacman.sh
+++ src/Scripts/pacman.sh
@@ -2,38 +2,40 @@
 
 # // ======= pacman.sh ========
 
-RED='\033[0;31m'
 CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
 RC='\033[0m'
 
-echo -e "${CYAN}Configuring pacman...${RC}"
-sleep 2
+echo "======================"
+echo -e "${YELLOW}Configuring PACMAN...${RC}"
+echo "======================"
+sleep 1
 
 if grep -Fq "#ParallelDownloads" /etc/pacman.conf; then
 	sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-	echo -e "${CYAN}Parallel downloads activated!${RC}"
+	echo -e "${CYAN}ParallelDownloads${RC} activated!"
 else
-	echo -e "${CYAN}ParallelDownloads is already enabled!${RC}"
+	echo -e "${CYAN}ParallelDownloads${RC} is already enabled!"
 fi
 
 if grep -Fxq "#Color" /etc/pacman.conf; then
 	sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
-	echo -e "${CYAN}Colors activated!${RC}"
+	echo -e "${CYAN}Colors${RC} activated!"
 else
-	echo -e "${RED}Failed to activate colors...${RC}"
+	echo -e "${CYAN}Colors${RC} are already enabled!"
 fi
 
 if grep -Fxq "#VerbosePkgLists" /etc/pacman.conf; then
 	sudo sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
-	echo -e "${CYAN}Verbose pkg lists activated!${RC}"
+	echo -e "${CYAN}VerbosePkgLists${RC} activated!"
 else
-	echo -e "${RED}Failed to activate verbose pkg lists...${RC}"
+	echo -e "${CYAN}VerbosePkgLists${RC} is already enabled!"
 fi
 
 if grep -Fxq "ILoveCandy" /etc/pacman.conf; then
-	echo -e "${RED}I love candy already activated...${RC}"
+	echo -e "${CYAN}I love candy${RC} is already enabled!"
 	exit
 else
 	sudo sed -i '/^ParallelDownloads = .*/a ILoveCandy' /etc/pacman.conf
-	echo -e "${CYAN}I love candy activated${RC}"
+	echo -e "${CYAN}I love candy${RC} activated"
 fi
