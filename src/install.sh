9d721417bb875e8a72ca7a07bb46172311579a9d
g5ostXa <g5ostX2@proton.me>
2025-10-26 18:12:53 -0400

    Update install.sh for (#204)

--- src/install.sh
+++ src/install.sh
@@ -13,68 +13,70 @@ HYPRARCH2_DIR="$HOME/Downloads/hyprarch2"
 VERSION_NAME="$HYPRARCH2_DIR/.version/latest"
 DOTS_TARGET_DIR="$HOME/dotfiles"
 H2INSTALLER_REPO="https://github.com/g5ostXa/h2install"
+H2INSTALLER_DIR="$HOME/Downloads/h2install"
 H2INSTALLER_TARGET_LOC="$HOME/Downloads/h2install/h2installer"
 
 if ! command -v "gum" >/dev/null; then
-    echo -e "${RED};; Dependency gum NOT FOUND... ${RC}"
-    exit 1
+	echo -e "${RED};; Dependency gum NOT FOUND... ${RC}"
+	exit 1
 fi
 
-# Installation greeter
-if command -v figlet >/dev/null 2>&1; then
-	clear
-	echo -e "${CYAN}"
-	figlet -f smslant "Installer"
-	echo "Welcome to hyprarch2"
-	cat "$VERSION_NAME"
-	echo -e "${RC}" && echo ""
-else
-	clear
-	echo -e "${CYAN}"
-	cat <<"EOF"
- ___           _        _ _
-|_ _|_ __  ___| |_ __ _| | | ___ _ __
- | || '_ \/ __| __/ _` | | |/ _ \ '__|
- | || | | \__ \ || (_| | | |  __/ |
-|___|_| |_|___/\__\__,_|_|_|\___|_|
+install_greeter() {
+	if command -v figlet >/dev/null 2>&1; then
+		clear
+		echo -e "${CYAN}"
+		figlet -f smslant "Installer"
+		echo "Welcome to hyprarch2"
+		cat "$VERSION_NAME"
+		echo -e "${RC}" && echo ""
+	else
+		clear
+		echo -e "${CYAN}"
+		cat <<"EOF"
+	 ___           _        _ _
+	|_ _|_ __  ___| |_ __ _| | | ___ _ __
+	 | || '_ \/ __| __/ _` | | |/ _ \ '__|
+	 | || | | \__ \ || (_| | | |  __/ |
+	|___|_| |_|___/\__\__,_|_|_|\___|_|
 EOF
-	echo "Welcome to hyprarch2"
-	cat "$VERSION_NAME"
-	echo -e "${RC}" && echo ""
-fi
+		echo "Welcome to hyprarch2"
+		cat "$VERSION_NAME"
+		echo -e "${RC}" && echo ""
+	fi
 
-# Simple prompt if installing via ssh
-if [ -n "$SSH_CONNECTION" ]; then
-	while true; do
-		read -r -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn):" yn
-		case $yn in
-		[Yy]*)
+	# Simple prompt if installing via ssh
+	if [ -n "$SSH_CONNECTION" ]; then
+		while true; do
+			read -r -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn):" yn
+			case $yn in
+			[Yy]*)
+				echo ";; Starting Installation..."
+				func_main || exit 1
+				break
+				;;
+			[Nn]*)
+				echo ";; Installation canceled..."
+				exit
+				;;
+			*)
+				echo ";; Please answer yes or no."
+				;;
+			esac
+		done
+	else
+		# Gum prompt if not installing via ssh
+		if gum confirm "DO YOU WANT TO START THE INSTALLATION NOW?"; then
 			echo ";; Starting Installation..."
-			sleep 2
-			break
-			;;
-		[Nn]*)
+			func_main || exit 1
+		elif [ $? -eq 130 ]; then
+			echo ";; Installation canceled..."
+			exit 130
+		else
 			echo ";; Installation canceled..."
 			exit
-			;;
-		*)
-			echo ";; Please answer yes or no."
-			;;
-		esac
-	done
-else
-	# Gum prompt if not installing via ssh
-	if gum confirm "DO YOU WANT TO START THE INSTALLATION NOW?"; then
-		echo ";; Starting Installation..."
-		sleep 2
-	elif [ $? -eq 130 ]; then
-		echo ";; Installation canceled..."
-		exit 130
-	else
-		echo ";; Installation canceled..."
-		exit
+		fi
 	fi
-fi
+}
 
 # Check if hyprarch2 directory exists
 src_check() {
@@ -127,10 +129,10 @@ func_main() {
 
 	if [ -d "$H2INSTALLER_DIR" ]; then
 		rm -rf "$H2INSTALLER_DIR"
-        cd "$HOME/Downloads" || exit 1
-    else
-        cd "$HOME/Downloads" || exit 1
-    fi
+		cd "$HOME/Downloads" || exit 1
+	else
+		cd "$HOME/Downloads" || exit 1
+	fi
 
 	git clone --depth=1 "$H2INSTALLER_REPO".git
 	cd h2install && rm -rf .git/ && go mod tidy && go build -o h2installer
@@ -146,9 +148,7 @@ func_main() {
 }
 
 # Script entry
-echo -e "${YELLOW};; INFO: You're about to clone the h2install repository${RC}"
-read -rp ";; Are you sure you want to start the installation now? [y/N]" ans
-[[ "$ans" =~ ^[Yy]$ ]] && func_main || exit 1
+install_greeter || exit 1
 
 # Check if essential files were copied
 echo -e "${YELLOW};; Verifying if all essential files are copied...${RC}"
