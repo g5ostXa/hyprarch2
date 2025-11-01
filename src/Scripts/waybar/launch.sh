15e83e0068021ef70ff6b693e404046520bb886d
g5ostXa <g5ostX2@proton.me>
2025-11-01 02:17:21 -0400

    waybar complete refactor commit-1

--- src/Scripts/waybar/launch.sh
+++ src/Scripts/waybar/launch.sh
@@ -6,23 +6,4 @@
 killall waybar
 sleep 0.2
 
-# Default theme: /THEMEFOLDER;/VARIATION
-themestyle="/waybar-bottom;/waybar-bottom/main"
-
-# Get current theme information from .cache/.themestyle.sh
-if [ -f ~/.cache/.themestyle.sh ]; then
-	themestyle=$(cat ~/.cache/.themestyle.sh)
-else
-	touch ~/.cache/.themestyle.sh
-	echo "$themestyle" >~/.cache/.themestyle.sh
-fi
-
-IFS=';' read -ra arrThemes <<<"$themestyle"
-echo ${arrThemes[0]}
-
-if [ ! -f ~/.config/waybar/themes${arrThemes[1]}/style.css ]; then
-	themestyle="/waybar-bottom;/waybar-bottom/main"
-fi
-
-# Loading the configuration
-uwsm app -- waybar -c ~/.config/waybar/themes${arrThemes[0]}/config -s ~/.config/waybar/themes${arrThemes[1]}/style.css &
+uwsm app -- waybar -c ~/.config/waybar/config.jsonc &
