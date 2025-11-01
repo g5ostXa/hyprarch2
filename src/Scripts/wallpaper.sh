b1fa6ab1811795a7cf766d914158416b1542996c
g5ostXa <g5ostX2@proton.me>
2025-11-01 09:33:58 -0400

    Update wallpaper.sh

--- src/Scripts/wallpaper.sh
+++ src/Scripts/wallpaper.sh
@@ -5,6 +5,9 @@
 # Get used wallpaper
 wallpaper="$1"
 
+# Define waybar launcher
+WAYBAR_LAUNCHER="$HOME/src/Scripts/waybar.sh"
+
 # Set wallpaper and load colors
 wal -q -i "$wallpaper"
 cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
@@ -12,3 +15,18 @@ cp "$wallpaper" "$HOME/.cache/current_wallpaper.jpg"
 # Get wallpaper name and send notification
 newwall=$(basename "$wallpaper")
 notify-send --urgency=normal "Wallpaper and colors updated!" "with image $newwall" --icon=folder-pictures
+
+# Reload waybar because it won't load when executed from autostart.conf
+if command -v "waybar" >/dev/null 2>&1; then
+	if ! pgrep -x "waybar" >/dev/null 2>&1; then
+		sleep 2
+		bash -e "$HOME/src/Scripts/waybar.sh"
+	else
+		killall waybar
+		sleep 0.75
+		bash -e "$HOME/src/Scripts/waybar.sh"
+	fi
+else
+	notify-send --urgency=critical "Waybar not found, aborting..."
+	exit 1
+fi
