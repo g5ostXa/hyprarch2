5c102f59d988e46a88670592ae0b41aceb786aa5
g5ostXa <g5ostX2@proton.me>
2025-11-03 19:27:32 -0500

    Updates

--- src/Scripts/hypr/hypridle.sh
+++ src/Scripts/hypr/hypridle.sh
@@ -6,7 +6,7 @@ SERVICE="hypridle"
 
 if pgrep -x "$SERVICE" >/dev/null; then
 	killall "$SERVICE"
-	notify-send --urgency=normal ":: $SERVICE DEACTIVATED" --icon=dialog-information
+	notify-send --urgency=normal ":: $SERVICE DEACTIVATED"
 else
 	uwsm app -- "$SERVICE" &
 	notify-send --urgency=normal ":: $SERVICE ACTIVATED"
