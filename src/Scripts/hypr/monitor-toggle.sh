5cc0146a02945015ea9ca5dbb7d47d82efab65b8
g5ostXa <g5ostX2@proton.me>
2025-11-04 23:16:56 -0500

    Updates

--- src/Scripts/hypr/monitor-toggle.sh
+++ src/Scripts/hypr/monitor-toggle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# // ==== monitor_toggle.sh ====
+# // ==== monitor-toggle.sh ====
 
 # Script to toggle laptop monitor on and off
 # Monitor(s) are define in .config/hypr/conf/environment.conf
@@ -10,15 +10,15 @@ CURRENT_STATUS=$(hyprctl monitors -j | jq -r ".[] | select(.name==\"$PRIMARY_MON
 func_main() {
 	if [ "$CURRENT_STATUS" = "true" ]; then
 		hyprctl dispatch dpms off "$PRIMARY_MONITOR"
-		notify-send --urgency=normal "DPMS turned OFF for $PRIMARY_MONITOR"
+		notify-send --urgency=normal ":: DPMS turned OFF for $PRIMARY_MONITOR"
 	else
 		hyprctl dispatch dpms on "$PRIMARY_MONITOR"
-		notify-send --urgency=normal "DPMS turned ON for $PRIMARY_MONITOR"
+		notify-send --urgency=normal ":: DPMS turned ON for $PRIMARY_MONITOR"
 	fi
 }
 
 if [[ -z "$CURRENT_STATUS" ]]; then
-	notify-send --urgency=critical "Monitor not found..." "Please set the environment variable LAPTOP_MONITOR_NAME in your .bashrc"
+	notify-send --urgency=critical ":: Monitor not found..." "Please set the environment variable LAPTOP_MONITOR_NAME in your .bashrc"
 	exit 1
 else
 	func_main
