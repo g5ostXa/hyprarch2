a1ea1e3b5d3ff4376bb4eda717b665d6bf4b15b2
g5ostXa <g5ostX2@proton.me>
2026-01-17 14:51:18 -0500

    Fixed waybar updates module (#216)

--- src/Scripts/updates.sh
+++ src/Scripts/updates.sh
@@ -3,9 +3,9 @@
 # // ======= updates.sh =======
 
 # Define waybar module color based on the number of updates
-threshold_green=0
-threshold_yellow=25
-threshold_red=100
+threshold_ok=0
+threshold_warning=24
+threshold_critical=99
 
 # Define which AUR helper is installed
 define_helper() {
@@ -31,22 +31,25 @@ get_number() {
 		updates_aur=0
 	fi
 
-	updates=$(("$updates_arch" + "$updates_aur")) && css_class="green"
+	updates=$(("$updates_arch" + "$updates_aur"))
 }
 
 define_helper && get_number
 
+# Define waybar module class based on the number of updates
+css_class="ok"
+
+if [ "$updates" -gt $threshold_warning ]; then
+	css_class="warning"
+fi
+
+if [ "$updates" -gt $threshold_critical ]; then
+	css_class="critical"
+fi
+
 # Print json format for waybar module
-if [ "$updates" -gt $threshold_yellow ]; then
-	css_class="yellow"
-fi
-
-if [ "$updates" -gt $threshold_red ]; then
-	css_class="red"
-fi
-
-if [ "$updates" -gt $threshold_green ]; then
+if [ "$updates" -gt $threshold_ok ]; then
 	printf '{"text": "%s", "alt": "%s", "tooltip": "%s Updates", "class": "%s"}' "$updates" "$updates" "$updates" "$css_class"
 else
-	printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "green"}'
+	printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "ok"}'
 fi
