83b7a68daf5ea153c79ec606901dccf0ec69b9b7
g5ostXa <g5ostX2@proton.me>
2026-04-23 14:58:15 -0400

    Update bwrap-btop.sh

--- src/Scripts/sandboxes/bwrap-btop.sh
+++ src/Scripts/sandboxes/bwrap-btop.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# // ======= bwrap-btop.sh =======
+# // ===== bwrap-btop.sh =====
 
 exec bwrap \
 	--ro-bind /usr /usr \
 	--ro-bind /bin /bin \
+	--ro-bind /etc /etc \
 	--ro-bind /lib /lib \
 	--ro-bind /lib64 /lib64 \
-	--ro-bind /sbin /sbin \
-	--ro-bind /etc /etc \
 	--dev /dev \
 	--proc /proc \
 	--tmpfs /tmp \
-	--ro-bind "$HOME" "$HOME" \
-	--bind "$HOME/.config/btop" "$HOME/.config/btop" \
-	--unshare-net \
 	--dir /run \
+	--dir "$HOME" \
+	--dir "$HOME/.config" \
+	--bind "$HOME/dotfiles/btop" "$HOME/.config/btop" \
 	/usr/bin/btop
