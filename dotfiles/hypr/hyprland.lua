dbc17d37435c2406c5e0f64799244e1c176f652e
g5ostXa <g5ostX2@proton.me>
2026-05-15 11:24:15 -0400

    Update hyprland.lua for (#248)

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -1,50 +1,50 @@
 -- [MONITORS]:
-    -- eDP-1:
-        hl.monitor({ 
-            output = "eDP-1", 
-            mode = "1920x1080@120.035", 
-            position = "2048x0", 
-            scale = 1 
+-- eDP-1:
+hl.monitor({
+    output = "eDP-1",
+    mode = "1920x1080@120.035",
+    position = "2048x0",
+    scale = 1
 })
 
-    -- HDMI-A-1:
-        hl.monitor({ 
-            output = "HDMI-A-1", 
-            mode = "2048x1080@59.998", 
-            position = "0x0", 
-            scale = 1 
+-- HDMI-A-1:
+hl.monitor({
+    output = "HDMI-A-1",
+    mode = "2048x1080@59.998",
+    position = "0x0",
+    scale = 1
 })
 
-    -- Virtual-1:
-        -- hl.monitor({ 
-        -- output = "Virtual-1", 
-        -- mode = "2048x1080@59.998", 
-        -- position = "0x0", 
-        -- scale = 1 
+-- Virtual-1:
+-- hl.monitor({
+-- output = "Virtual-1",
+-- mode = "2048x1080@59.998",
+-- position = "0x0",
+-- scale = 1
 -- })
 
 -- [ENVIRONMENT]:
-    -- General:
-        hl.env("XCURSOR_SIZE", "24")
-        hl.env("QT_QPA_PLATFORM", "wayland")
-        hl.env("GTK_THEME", "Tokyonight-Dark:dark")
-    
-    -- XDG desktop portal:
-        hl.env("XDG_CURRENT_DESKTOP", "hyprland")
-        hl.env("XDG_SESSION_TYPE", "wayland")
-        hl.env("XDG_SESSION_DESKTOP", "hyprland")
-    
-    -- Monitors:
-        hl.env("PRIMARY_MONITOR", "eDP-1")
-        hl.env("SECONDARY_MONITOR", "HDMI-A-1")
-     -- hl.env("VIRTUAL_MONITOR", "Virtual-1")
+-- General:
+hl.env("XCURSOR_SIZE", "24")
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("GTK_THEME", "Tokyonight-Dark:dark")
 
-    -- GTK:
-        hl.env("GKD_SCALE", "1")
+-- XDG desktop portal:
+hl.env("XDG_CURRENT_DESKTOP", "hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "hyprland")
 
-    -- Default browser:
-        hl.env("BROWSER", "firefox-nightly")
-        hl.env("MOZ_ENABLE_WAYLAND", "1")
+-- Monitors:
+hl.env("PRIMARY_MONITOR", "eDP-1")
+hl.env("SECONDARY_MONITOR", "HDMI-A-1")
+-- hl.env("VIRTUAL_MONITOR", "Virtual-1")
+
+-- GTK:
+hl.env("GKD_SCALE", "1")
+
+-- Default browser:
+hl.env("BROWSER", "firefox-nightly")
+hl.env("MOZ_ENABLE_WAYLAND", "1")
 
 -- [AUTOSTART]:
 hl.on("hyprland.start", function()
@@ -77,12 +77,12 @@ hl.config({
         active_opacity = 1.0,
         inactive_opacity = 1.0,
         fullscreen_opacity = 1.0,
-        
+
         blur = {
             enabled = true,
             size = 6,
             passes = 2,
-            new_optimizations = true, -- Use true/false instead of "on"
+            new_optimizations = true,
             ignore_opacity = true,
             xray = true,
         },
@@ -101,7 +101,7 @@ hl.config({
         touchpad = {
             natural_scroll = false,
         },
-        sensitivity = 0, 
+        sensitivity = 0,
     },
 
     gestures = {
@@ -115,7 +115,7 @@ hl.config({
         pseudotile = true,
         preserve_split = true,
     },
-    
+
     master = {
         new_status = "master",
     },
@@ -126,3 +126,45 @@ hl.config({
         pass_mouse_when_bound = false,
     },
 })
+
+-- [ANIMATIONS]:
+hl.config({
+    animations = {
+        enabled = true,
+        bezier = {
+            "wind, 0.05, 0.9, 0.1, 1.05",
+            "winIn, 0.1, 1.1, 0.1, 1.1",
+            "winOut, 0.3, -0.3, 0, 1",
+            "liner, 1, 1, 1, 1",
+        },
+        animation = {
+            "windows, 1, 6, wind, slide",
+            "windowsIn, 1, 6, winIn, slide",
+            "windowsOut, 1, 5, winOut, slide",
+            "windowsMove, 1, 5, wind, slide",
+            "border, 1, 1, liner",
+            "borderangle, 1, 30, liner, loop",
+            "fade, 1, 10, default",
+            "workspaces, 1, 5, wind",
+        },
+    },
+})
+
+--[[
+hl.config({
+    animations = {
+        enabled = true,
+        bezier = {
+            "myBezier, 0.05, 0.9, 0.1, 1.05",
+        },
+        animation = {
+            "windows, 1, 7, myBezier",
+            "windowsOut, 1, 7, default, popin 80%",
+            "border, 1, 10, default",
+            "borderangle, 1, 8, default",
+            "fade, 1, 7, default",
+            "workspaces, 1, 6, default",
+        },
+    },
+})
+--]]
